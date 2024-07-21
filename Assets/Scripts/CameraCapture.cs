13b5a7e3a29833414112aeafa7c37994cad7d574
dlgkor <dlgkor05@gmail.com>
2024-07-21 18:41:24 +0900

    Add StartCaptureAndSaveImage function

--- Assets/Scripts/CameraCapture.cs
+++ Assets/Scripts/CameraCapture.cs
@@ -10,12 +10,35 @@ public class CameraCapture : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(CaptureAndSaveImage());  
+        
         // renderTexture 변수에 Render Texture를 할당해야 합니다.
         // renderTexture = someRenderTexture;
     }
 
+    public void StartCaptureAndSaveImage()
+    {
+        StartCoroutine(CaptureAndSaveImage());
+    }
+
     IEnumerator CaptureAndSaveImage()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Render Texture 변수에서 이미지 읽어오기
+        RenderTexture.active = renderTexture;
+        Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        texture2D.Apply();
+
+        // 읽어온 이미지 JPEG 포맷으로 디코딩
+        byte[] bytes = texture2D.EncodeToJPG();
+
+        // Clean up
+        RenderTexture.active = null;
+        Destroy(texture2D);
+    }
+
+    IEnumerator CaptureAndSaveImageElapse()
     {
         yield return new WaitForEndOfFrame();
         System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch(); // Stopwatch 객체 생성
@@ -32,12 +55,6 @@ public class CameraCapture : MonoBehaviour
 
         stopwatch.Stop(); // 알고리즘 실행 시간 측정 종료
         Debug.Log("Elapsed milliseconds: " + stopwatch.ElapsedMilliseconds); // 실행 시간 출력
-
-        // 디코딩 결과 byte 자료형 배열에 저장
-        //string path = Path.Combine(Application.dataPath, "CapturedImage.jpg");
-        //File.WriteAllBytes(path, bytes);
-
-        //Debug.Log("Image saved to: " + path);
 
         // Clean up
         RenderTexture.active = null;
