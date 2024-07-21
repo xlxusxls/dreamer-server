ab0ff57ea07f6accacd4f93ed9fca92f2795e694
dlgkor <dlgkor05@gmail.com>
2024-07-21 18:52:15 +0900

    Annotated uneccessary code

--- Assets/Scripts/CameraCapture.cs
+++ Assets/Scripts/CameraCapture.cs
@@ -15,7 +15,7 @@ public class CameraCapture : MonoBehaviour
         
         gameObject.GetComponent<Camera>().targetTexture = renderTexture;
 
-        StartCaptureAndSaveImage();
+        //StartCaptureAndSaveImage();
     }
 
     public void StartCaptureAndSaveImage()
@@ -37,10 +37,10 @@ public class CameraCapture : MonoBehaviour
         byte[] bytes = texture2D.EncodeToJPG();
 
         // 디코딩 결과 byte 자료형 배열에 저장
-        string path = Path.Combine(Application.dataPath, "CapturedImage.jpg");
-        File.WriteAllBytes(path, bytes);
+        //string path = Path.Combine(Application.dataPath, "CapturedImage.jpg");
+        //File.WriteAllBytes(path, bytes);
 
-        Debug.Log("Image saved to: " + path);
+        //Debug.Log("Image saved to: " + path);
 
         // Clean up
         RenderTexture.active = null;
